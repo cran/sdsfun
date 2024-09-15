08dec11e9f69415f7ae7824ba139d1ccae826df1
Wenbo Lv <lyu.geosocial@gmail.com>
2024-09-15 04:30:02 +0000

    version 0.2.0

--- R/sf_helpers.R
+++ R/sf_helpers.R
@@ -81,3 +81,41 @@ sf_voronoi_diagram = \(sfj){
 
   return(sfj_voronoi)
 }
+
+#' @title generates distance matrix
+#' @description
+#' Generates distance matrix) for sf object
+#'
+#' @param sfj An `sf` object.
+#'
+#' @return A matix.
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' pts = read_sf(system.file('extdata/pts.gpkg',package = 'sdsfun'))
+#' pts_distm = sf_distance_matrix(pts)
+#' pts_distm[1:5,1:5]
+#'
+sf_distance_matrix = \(sfj){
+  .check_spwt(sfj)
+
+  if (sf_geometry_type(sfj) %in% c('multipoint','multipolygon')){
+    suppressWarnings({sfj = sf::st_point_on_surface(sfj)})
+  } else if (sf_geometry_type(sfj) == 'polygon') {
+    suppressWarnings({sfj = sf::st_centroid(sfj)})
+  }
+
+  coords = sfj %>%
+    sf::st_coordinates() %>%
+    {.[,c('X','Y')]}
+
+  longlat = dplyr::if_else(sf::st_is_longlat(sfj),TRUE,FALSE,FALSE)
+  if (longlat) {
+    distij = stats::as.dist(geosphere::distm(coords))
+  } else {
+    distij = stats::dist(as.data.frame(coords))
+  }
+
+  return(as.matrix(distij))
+}
